0cca2dac2778e2dbd826cb956c4110faea97d9a9
allachetan <43319083+allachetan@users.noreply.github.com>
2024-01-12 23:02:50 +0000

    save before refractor back to js

--- src/components/widget/Widget.tsx
+++ src/components/widget/Widget.tsx
@@ -1,10 +1,13 @@
-import { Mail } from "lucide-react";
-import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs"
+import { Mail, CalendarDays, ScrollText } from "lucide-react";
+import testlogo from "../../assets/testlogo.png";
+import { Input } from "@/components/ui/input"
+import { Label } from "@/components/ui/label"
+import { Button } from "@/components/ui/button";
 
 function Widget() {
 
     return (
-        <div className="fixed z-50 bottom-0 right-0 mb-4 mr-4 flex flex-col gap-4 items-end">
+        <div className="fixed z-50 bottom-0 right-0 mb-8 mr-8 flex flex-col gap-4 items-end">
             {
                 /*
                 <div className="bg-white h-[600px] w-80 rounded-lg flex flex-col">
@@ -18,20 +21,55 @@ function Widget() {
             </div>
                 */
             }
-            <Tabs defaultValue="account" className="w-[400px]">
-                <TabsList>
-                    <TabsTrigger value="account">Account</TabsTrigger>
-                    <TabsTrigger value="password">Password</TabsTrigger>
-                </TabsList>
-                <TabsContent value="account">Make changes to your account here.</TabsContent>
-                <TabsContent value="password">Change your password here.</TabsContent>
-            </Tabs>
+
+            <div className="bg-white min-h-[600px] w-[350px] rounded-lg flex flex-col shadow-xl overflow-hidden">
+                <div className="bg-amber-100 p-6 px-8 flex flex-col gap-2 items-center">
+                    <div className="border-[1px] border-gray-300 bg-white/50 rounded-xl flex gap-1 overflow-hidden w-fit">
+                        <div className="flex gap-2 py-1.5 px-3 items-center justify-center bg-amber-200 rounded-xl m-0.5">
+                            <CalendarDays className="w-4" />
+                            <p className="text-sm">Consultation</p>
+                        </div>
+
+                        <div className="flex gap-2 py-1.5 px-3 items-center justify-center rounded-xl m-0.5">
+                            <ScrollText className="w-4" />
+                            <p className="text-sm">Answers</p>
+                        </div>
+                    </div>
+
+                    <img src={testlogo} className="h-24 mt-4" />
+
+                    <div className="flex flex-col pt-2">
+                        <p className="font-medium text-2xl">Free Event Consultation</p>
+                        <p className="font-regular text-xs text-gray-600">We usually respond within 24 hours.</p>
+                    </div>
+                </div>
+
+                <div className="flex flex-col py-8 px-6 gap-4">
+                    <div className="flex flex-col w-full items-start gap-1.5">
+                        <Label htmlFor="name" className="text-gray-700">Name</Label>
+                        <Input type="text" id="name" placeholder="Enter your name" />
+                    </div>
+
+                    <div className="flex flex-col w-full items-start gap-1.5">
+                        <Label htmlFor="email" className="text-gray-700">Email address</Label>
+                        <Input type="email" id="email" placeholder="Email" />
+                    </div>
+
+                    <div className="flex flex-col w-full items-start gap-1.5">
+                        <Label htmlFor="tel" className="text-gray-700">Phone number</Label>
+                        <Input type="tel" id="tel" placeholder="Optional" />
+                    </div>
+
+                    <Button className="shadow-md hover:bg-amber-300 mt-4">Continue</Button>
+
+                </div>
+            </div>
 
             <button
-                className="bg-yellow-200 w-14 h-14 rounded-full flex justify-center p-0"
+                className="bg-yellow-200 w-14 h-14 rounded-full flex justify-center p-0 shadow-lg"
                 onClick={() => console.log('clicked')}
             >
-                <Mail className="w-8 h-8 m-auto text-black" />
+                <Mail className="w-[28px] h-[28px] m-auto text-black" />
             </button>
         </div>
     )
